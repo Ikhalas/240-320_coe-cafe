556431fd788a5ecbc365e5146b957d1128239ccb
Ikhalas <5910110405@psu.ac.th>
2018-09-24 15:43:44 +0700

    login page

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+        
+
+      <br></br>
+        <div className="page">  
+            <br></br> 
+            <img src ='https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/3176472/580/390/m1/fpnw/wm0/1-.png?1504000065&s=e9ced1ccd1d91c45aa61478a69a98d18'/> 
+            <h1>Please enter your name</h1>
+            <input></input><br></br><br></br>  
+            <button>Sign-in</button>
+            <br></br> 
+            <br></br> 
+        </div>
+
+
+      <br></br>
+      </div>
+    );
+  }
+}
+
+export default App;
