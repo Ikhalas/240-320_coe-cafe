c677aeaaf18d86c07244103a967a7a7bcba471cc
Ikhalas <5910110405@psu.ac.th>
2018-09-26 18:14:55 +0700

    backup ก่อนทดลองเปลี่ยน react router  version 4

--- src/App.js
+++ src/App.js
@@ -1,30 +1,91 @@
 import React, { Component } from 'react';
-import home from './home';
+import FacebookLogin from 'react-facebook-login';
 import './App.css';
 
-
 class App extends Component {
+  state = {
+    isLoggedIn: false,
+    userID: '',
+    name: '',
+    email: '',
+    picture: ''
+  }
+
+  responseFacebook = response =>{
+    //console.log(response);
+
+    this.setState({
+      isLoggedIn: true,
+      userID: response.userID,
+      name: response.name,
+      email: response.email,
+      picture: response.picture.data.url
+    });
+  }
+
+  componentClicked = () => console.log("clicked");
+
+
   render() {
+
+    let fbContent;
+
+    if(this.state.isLoggedIn){
+      fbContent = (
+        <div className="facebookIcon">
+          <br></br>
+          <img src={this.state.picture}/>
+          <p>you are logged in</p>
+          <p>{this.state.name}</p>
+          <div className="buttonFacebook"><a href="home">go to shop</a></div>
+          <home value = {this.state.name}/>
+          
+        </div>
+      );
+    }
+    else{
+      fbContent = (<FacebookLogin
+        appId="575653222895486"
+        autoLoad={true}
+        fields="name,email,picture"
+        onClick={this.componentClicked}
+        callback={this.responseFacebook} />)
+    }
+
     return (
       <div className="padding">
-      <br></br>
         <div className="box-login">  
-            <br></br> 
-            <img src ='https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/3176472/580/390/m1/fpnw/wm0/1-.png?1504000065&s=e9ced1ccd1d91c45aa61478a69a98d18'/> 
-            <h1>Please enter your name</h1>
-            <input></input><br></br><br></br>  
 
-            <div className="button">
-            <a href="home">Sign-in</a>
-            </div>
+            <br></br> 
+            <img src ='../logo.jpg'/> 
+            <br></br><br></br>
             
+            
+
+            <div>{fbContent}</div>
+            
+            <br></br><br></br><br></br>
+            <div className="buttonGuest">
+            <a href="home">guest login</a>
+            </div>
+           
+
+            
+
+            <br></br>
+
+            
+        
             <br></br> 
             <br></br> 
         </div>
 
 
       <br></br>
+      
       </div>
+      
+
     );
   }
 }
--- src/home.js
+++ src/home.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
 import './App.css';
 
 
 
 
 class home extends Component {
+    state = {
+        isLoggedIn: false,
+        userID: '',
+        name: '',
+        email: '',
+        picture: ''
+      }
+    
+      responseFacebook = response =>{
+        //console.log(response);
+    
+        this.setState({
+          isLoggedIn: true,
+          userID: response.userID,
+          name: response.name,
+          email: response.email,
+          picture: response.picture.data.url
+        });
+      }
+    
+      componentClicked = () => console.log("clicked");
+
+
     render() {
+
+        let fbContent;
+
+    if(this.state.isLoggedIn){
+      fbContent = (
+        <div className="welcomeBox">
+          <p>welcome</p>
+          <img src={this.state.picture}/>
+          <p>{this.state.name}</p>
+        </div>
+      );
+    }
+    else{
+      fbContent = (<FacebookLogin
+        appId="575653222895486"
+        autoLoad={true}
+        fields="name,email,picture"
+        onClick={this.componentClicked}
+        callback={this.responseFacebook} />)
+    }
+        
         return (
             <div className="padding">
                 <div className="page">
                     <br></br>
+                    <div>{fbContent}</div>
+                    <br></br>
                     <img src ='https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/3176472/580/390/m1/fpnw/wm0/1-.png?1504000065&s=e9ced1ccd1d91c45aa61478a69a98d18'/> 
                     <br></br>
-                    <coffee1/>
+                    
+                   
+                    
                     
                 </div>
                
